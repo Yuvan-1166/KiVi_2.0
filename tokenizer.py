cd0df6a622f4e225d0891a58e9f3bad00fa44738
JRBros2346 <jrbros2346@gmail.com>
2024-12-12 00:35:42 +0530

    [tokenizer] Complete

---
+++ tokenizer.py
@@ -0,0 +1,9 @@
+import sentencepiece as spm
+
+class TokenizerModel:
+    def __init__(self, model: str):
+        self.sp = spm.SentencePieceProcessor(model_file=model)
+    def encode(self, text: str) -> str:
+        return self.sp.encode(text)
+    def decode(self, tokens: list) -> str:
+        return self.sp.decode(tokens)
